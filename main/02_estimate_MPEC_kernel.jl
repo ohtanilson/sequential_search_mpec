65201a0b349ab2b75cebcd49f692468188b4bebd
ohtanilson <suguru.ohtani.n@gmail.com>
2024-07-31 10:48:59 +0900

    added scale check code

--- main/02_estimate_MPEC_kernel.jl
+++ main/02_estimate_MPEC_kernel.jl
@@ -5,54 +5,65 @@ Distributed.@everywhere include("../main/00_setting_julia.jl")
 Distributed.@everywhere include("../main/00_functions.jl")
 
 # setup for MPEC
-maxtime = 600.0
+maxtime = 300.0 #600.0
 max_iter = 500
 tol = 1e-3
-scaling = [-20, -20, -20]
 D = 100
-# @time res = Kernel_MPEC(data,maxtime,max_iter,tol,1)
 simulation_num = 50
-
+scaling = [-20, -20, -20]
+N_cons_vec = [10^3] #[10^3, 2*10^3]
+# @time res = Kernel_MPEC(data,maxtime,max_iter,tol,1)
 #results_MPEC = zeros(100, 7)
-for N_cons = [10^3,2*10^3]
-    @show N_cons
-    results_MPEC = zeros(simulation_num, 7)
-    results_MPEC_term = []
-    fin = []
-    filename_begin = "../sequential_search_mpec/output/sim_data"
-    filename_end   = ".csv"
-    filename = filename_begin*"_consumer_"*string(N_cons)*"_error_draw_"*string(D)*filename_end
-    data_all = CSV.read(filename, DataFrame) 
-    @time @threads for i = 1:simulation_num # for parallel computation
-    #@time for i = 1:simulation_num #100 #
-        #data
-        data = data_all[data_all[:, 1] .== i, 2:end] |> Matrix{Float64}    
-        run_time = @elapsed begin 
-                res_mpec = Kernel_MPEC(data,maxtime,max_iter,tol,i)
-            end
-        
-        results_MPEC[i, 1:5] .= [res_mpec[1];log(res_mpec[2])] 
-        results_MPEC[i, 6:6] .= res_mpec[4]
-        results_MPEC[i, 7:7] .= run_time 
-        append!(results_MPEC_term,[res_mpec[5]]) 
-        
-        append!(fin, i)
-        println("finished: ", length(fin), "/", 100)
-        GC.gc()
-        GC.gc()
+function estimate_kernel_MPEC(maxtime,max_iter,tol,D,simulation_num,scaling, N_cons_vec)
+    for N_cons = N_cons_vec
+        @show N_cons
+        results_MPEC = zeros(simulation_num, 7)
+        results_MPEC_term = []
+        fin = []
+        filename_begin = "../sequential_search_mpec/output/sim_data"
+        filename_end   = ".csv"
+        filename = filename_begin*"_consumer_"*string(N_cons)*"_error_draw_"*string(D)*filename_end
+        data_all = CSV.read(filename, DataFrame) 
+        #@time @threads for i = 1:simulation_num # for parallel computation
+        @time for i = 1:simulation_num #100 #
+            #data
+            data = data_all[data_all[:, 1] .== i, 2:end] |> Matrix{Float64}    
+            run_time = @elapsed begin 
+                    res_mpec = Kernel_MPEC(data,maxtime,max_iter,tol,i)
+                end
+            
+            results_MPEC[i, 1:5] .= [res_mpec[1];log(res_mpec[2])] 
+            results_MPEC[i, 6:6] .= res_mpec[4]
+            results_MPEC[i, 7:7] .= run_time 
+            append!(results_MPEC_term,[res_mpec[5]]) 
+            
+            append!(fin, i)
+            println("finished: ", length(fin), "/", 100)
+            GC.gc()
+            GC.gc()
+        end
+    
+        results_MPEC_df = DataFrame(hcat(results_MPEC[1:simulation_num,:], results_MPEC_term),:auto)
+        #column names
+        rename!(results_MPEC_df, names(results_MPEC_df) .=> ["beta1", "beta2", "beta3", "beta4", "logc", "loglik", "time", "converged"])
+        filename_begin = "../sequential_search_mpec/output/results_MPEC"
+        filename_end   = ".csv"
+        filename = filename_begin*"_consumer_"*string(N_cons[1])*"_error_draw_"*string(D)*"_scaling_"*string(scaling[1])*string(scaling[2])*string(scaling[3])*filename_end
+        CSV.write(filename, results_MPEC_df, writeheader=false)
+    
     end
-
-    results_MPEC_df = DataFrame(hcat(results_MPEC[1:simulation_num,:], results_MPEC_term),:auto)
-    #column names
-    rename!(results_MPEC_df, names(results_MPEC_df) .=> ["beta1", "beta2", "beta3", "beta4", "logc", "loglik", "time", "converged"])
-    filename_begin = "../sequential_search_mpec/output/results_MPEC"
-    filename_end   = ".csv"
-    filename = filename_begin*"_consumer_"*string(N_cons)*"_error_draw_"*string(D)*filename_end
-    CSV.write(filename, results_MPEC_df, writeheader=false)
-
+    return results_MPEC_df
 end
 
+@time results_MPEC_df = 
+    estimate_kernel_MPEC(maxtime,max_iter,tol,D,simulation_num,scaling, N_cons_vec)
 
+scaling = [-18, -4, -7]# ursu et al (2023)
+@time results_MPEC_df = 
+    estimate_kernel_MPEC(maxtime,max_iter,tol,D,simulation_num,scaling, N_cons_vec)
+scaling = [-50, -50, -50]# ursu et al (2023)
+@time results_MPEC_df = 
+    estimate_kernel_MPEC(maxtime,max_iter,tol,D,simulation_num,scaling, N_cons_vec)
 
 param = [1.0, 0.7, 0.5, 0.3, -3.0]
 mean(results_MPEC_df[:,1] .- param[1])
