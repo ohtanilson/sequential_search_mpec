c4d1ffe47d05c4d95154a90609d6793f2b028cca
ohtanilson <suguru.ohtani.n@gmail.com>
2024-09-10 09:23:56 +0900

    finalized for WP

--- seq_code/seq_code/crude/multiply.m
+++
@@ -1,6 +0,0 @@
-clc; clear all;
-for i=2:50
-    filename = sprintf('estWeitz_crude_D100S1.m',i);
-    filename2=sprintf('estWeitz_crude_D100S%d.m',i);
-    copyfile(filename,filename2)
-end
--- seq_code/seq_code/importance_sampling/multiply.m
+++
@@ -1,6 +0,0 @@
-clc; clear all;
-for i=2:50
-    filename = sprintf('estWeitz_imp_D100M100S1.m',i);
-    filename2=sprintf('estWeitz_imp_D100M100S%d.m',i);
-    copyfile(filename,filename2)
-end
